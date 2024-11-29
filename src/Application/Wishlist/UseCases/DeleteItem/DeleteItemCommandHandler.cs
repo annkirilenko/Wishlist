2f275c8c22c88c22f29d280e2daa6db414c738c6
mori <l.yophen.l@gmail.com>
2024-11-29 18:14:56 +0300

    Implemented the delete command

--- src/Application/Wishlist/UseCases/DeleteItem/DeleteItemCommandHandler.cs
+++ src/Application/Wishlist/UseCases/DeleteItem/DeleteItemCommandHandler.cs
@@ -1,9 +1,26 @@
+using Wishlist.Domain.Entities;
+using Wishlist.Domain.Repositories;
+
 namespace Wishlist.Application.Wishlist.UseCases.DeleteItem;
 
 public class DeleteItemCommandHandler
 {
+    private readonly IWishlistItemRepository _wishlistItemRepository;
+
+    public DeleteItemCommandHandler(IWishlistItemRepository wishlistItemRepository)
+    {
+        _wishlistItemRepository = wishlistItemRepository;
+    }
+
     public async Task Handle(DeleteItemCommand command)
     {
+        var wishlistItem = await _wishlistItemRepository.FindOneByItemIdAsync(command.ItemId);
+
+        if (wishlistItem is null)
+        {
+            throw new InvalidOperationException("Wishlist item not found");
+        }
         
+        await _wishlistItemRepository.DeleteAsync(wishlistItem);
     }
 }
